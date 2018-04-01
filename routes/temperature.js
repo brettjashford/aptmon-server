3196bd2181c62da47787ebf1c87e991c111ef4d8
Brett Ashford <brettjashford@gmail.com>
2018-04-01 19:57:25 -0400

    save temps endpoint

--- routes/temperature.js
+++ routes/temperature.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function (req, res) {
-    res.json(req.body);
-})
+router.post('/', async function (req, res) {
+    const temps = req.body;
+    if (!Array.isArray(temps)) {
+        throw new Error('array expected');
+    }
+
+    // TODO: better error handling, logging
+    temps.map(temp => new Promise((resolve, reject) => {
+        req.app.locals.db.collection('temperature').insertOne({
+            ...temp,
+            dateAdded: new Date()
+        }, (error, response) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(response);
+        });
+    }));
+
+    await Promise.all(temps);
+
+    res.json({
+        success: true,
+        numAdded: temps.length
+    });
+});
 
 module.exports = router;
